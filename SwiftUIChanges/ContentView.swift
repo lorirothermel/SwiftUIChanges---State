3d57f7e953cf48d067093ba8a817678a101197af
Lori Rothermel <lori@rothermel1.com>
2024-12-12 17:29:04 -0800

    @State

--- SwiftUIChanges/ContentView.swift
+++ SwiftUIChanges/ContentView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+    @State private var isOn: Bool = false
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            
+            Toggle(isOn: $isOn) {
+                Text("Light")
+                    .font(.largeTitle)
+            }  // Toggle
+            .tint(.red)
+            .padding(.bottom, 25)
+            
+            Text("\(count)")
+                .font(.largeTitle)
+            
+            Button(isOn ? "ON - Increment" : "OFF") {
+                count = count + 1
+            }  // Button
+            .buttonStyle(.borderedProminent)
+        }  // VStack
         .padding()
-    }
-}
+        
+    }  // some View
+    
+}  // ContentView
 
 #Preview {
     ContentView()
